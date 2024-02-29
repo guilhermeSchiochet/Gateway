aca5f2f81cdd66df82322d2b712725534a200143
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2024-02-28 22:18:38 -0300

    Create gateway.dart

---
+++ gateway/lib/gateway.dart
@@ -0,0 +1,9 @@
+library gateway;
+
+export 'src/core/gateway_client.dart';
+export 'src/core/request_interceptor.dart';
+export 'src/core/response_interceptor.dart';
+export 'src/models/request_model.dart';
+export 'src/models/response_model.dart';
+export 'src/models/error_model.dart';
+export 'src/utils/network_utils.dart';
